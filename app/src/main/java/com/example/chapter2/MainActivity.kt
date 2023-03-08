3c6ec9f345dcc7125c22ad188459dce9e027a778
nurget <101033246+nurget@users.noreply.github.com>
2023-03-08 12:01:29 +0900

    숫자 세기 앱 - 구현 완료

--- app/src/main/java/com/example/chapter2/MainActivity.kt
+++ app/src/main/java/com/example/chapter2/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+
+
+        var number = 0
+
+        resetButton.setOnClickListener {
+            number = 0
+            numberTextView.text = number.toString()
+            Log.d("onClick", "숫자는 $number")
+        }
+
+        plusButton.setOnClickListener {
+            number += 1 // number = number + 1과 동일함.
+            numberTextView.text = number.toString()
+            Log.d("onClick", "플러스 된 숫자는 $number")
+        }
     }
 }
