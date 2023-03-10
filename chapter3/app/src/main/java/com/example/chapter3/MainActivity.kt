b1d936e78ee823d376f7d0bee7cc583d3442c354
nurget <101033246+nurget@users.noreply.github.com>
2023-03-10 13:21:50 +0900

    단위변환기 기능 구현 완료 및 에러 처리

--- chapter3/app/src/main/java/com/example/chapter3/MainActivity.kt
+++ chapter3/app/src/main/java/com/example/chapter3/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.chapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import com.example.chapter3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val outputTextView = binding.outputTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val inputEditText = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
+        val swapImageButton = binding.swapImageButton
+
+        var inputNumber : Int = 0
+        var cmToM = true
+
+        inputEditText.addTextChangedListener {text ->
+            inputNumber = if(text.isNullOrEmpty()) {
+                0
+        } else {
+            text.toString().toInt()
+        }
+            var outputNumber = inputNumber.times(0.01)
+            if(cmToM) {
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
+        swapImageButton.setOnClickListener {
+            cmToM = cmToM.not() // 같은 내용은 !cmToM
+            if(cmToM) {
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
     }
 }
