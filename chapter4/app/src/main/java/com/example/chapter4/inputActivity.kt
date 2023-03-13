c6ea98c1f1bf639107fdc1880cd5fff5ab9fcfbf
nurget <101033246+nurget@users.noreply.github.com>
2023-03-13 23:04:36 +0900

    응급의료 정보 앱 activity 추가

---
+++ chapter4/app/src/main/java/com/example/chapter4/inputActivity.kt
@@ -0,0 +1,13 @@
+package com.example.chapter4
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chapter4.databinding.ActivityMainBinding
+
+class inputActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.input_activity)
+    }
+}
