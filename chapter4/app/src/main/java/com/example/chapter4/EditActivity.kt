20dbee8558c971a1ea04dc2a66fbb4f2cf7ed9fb
nurget <101033246+nurget@users.noreply.github.com>
2023-03-16 21:31:34 +0900

    datepicker, checkbox 기능 구현

--- chapter4/app/src/main/java/com/example/chapter4/EditActivity.kt
+++ chapter4/app/src/main/java/com/example/chapter4/EditActivity.kt
@@ -1,9 +1,13 @@
 package com.example.chapter4
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import com.example.chapter4.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
@@ -19,5 +23,24 @@ class EditActivity : AppCompatActivity() {
             android.R.layout.simple_list_item_1
         )
 
+        binding.birthdateLayer.setOnClickListener {
+        val listener = OnDateSetListener{
+            _, year, month, dayOfMonth ->
+            binding.birthdateTextView.text = "$year-${month.inc()}-$dayOfMonth"
+        }
+            DatePickerDialog(
+                this,
+                listener,
+                2000,
+                1,
+                1
+            ).show()
+        }
+
+        binding.warningCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            binding.precautionsEditText.isVisible = isChecked
+        }
+
+        binding.precautionsEditText.isVisible = binding.warningCheckBox.isChecked
     }
 }
